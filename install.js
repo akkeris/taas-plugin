3bd9df67e580ec3601b04befa919c2680015ea3b
Sam Beckett <becketts@byu.edu>
2020-01-30 14:57:02 -0700

    take current runs out of beta

--- install.js
+++ install.js
@@ -2,4 +2,6 @@ const proc = require('child_process');
 
 const isWindows = process.platform === 'win32';
 
-proc.spawnSync('npm', ['install'], {cwd:__dirname, env:process.env, stdio:'ignore', shell: isWindows || undefined});
+proc.spawnSync('npm', ['install'], {
+  cwd: __dirname, env: process.env, stdio: 'ignore', shell: isWindows || undefined,
+});
